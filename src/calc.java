a81a2acfd56aefc32ef7d12b6be301761717d445
Fabio Batista <fabio.santos@seati.ma.gov.br>
2018-06-26 15:42:57 -0300

    Ajustes nos paramentros.

--- src/calc.java
+++ src/calc.java
@@ -1,18 +1,18 @@
 
 public class calc {
 	
-	private static double Xcg = 10.61;
-	private static double Scg = 4.73;
+	private static double Xcg = 7.85;
+	private static double Scg = 3.06;
 	private static int pesocg = 1;
-
-	private static double Xce = 13.55;
-	private static double Sce = 3.17;
+	
+	private static double Xce = 12.76;
+	private static double Sce = 3.39;
 	private static int pesoce = 3;
 	
 	public static void main(String[] args) {
 		
-		int Acg = 6;
-		int Ace = 15;
+		int Acg = 7;		
+		int Ace = 16;
 		
 		double npcg = 74.21;
 		double npce = 178.18;
